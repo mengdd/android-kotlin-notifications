360ec6614cb048d56371076fdf8c7f8b10b38b77
Dandan Meng <mengdandanno1@163.com>
2019-12-03 09:08:14 +0800

    Lesson 2-10: cancel the previous notification when start timer

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -84,4 +84,11 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
 }
 
-// TODO: Step 1.14 Cancel all notifications
+// Step 1.14 Cancel all notifications
+/**
+ * Cancels all notifications.
+ *
+ */
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
